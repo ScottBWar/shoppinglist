053b861dd7583b2873e17f5956d8ca8e6c0bab81
Scott Warner <scottbwarner@gmail.com>
2015-10-30 11:09:28 -0400

    delete route works correctly

--- server.js
+++ server.js
@@ -8,18 +8,17 @@ var Storage = function() {
 };
 
 Storage.prototype.add = function(name) {
-    var item = {name: name, id: this.id};
+    var item = {
+        name: name,
+        id: this.id
+    };
     this.items.push(item);
     this.id += 1;
     return item;
 };
 
-Storage.prototype.delete = function(id){
-	
-	console.log(this.items);
-	console.log(this.items[id]);
-	this.items.splice(id,1);
-
+Storage.prototype.delete = function(id) {
+    // return this.items.splice(id,1);
 };
 
 var storage = new Storage();
@@ -31,31 +30,32 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/items', function(req, res) {
+		console.log(storage.items);
     res.json(storage.items);
 });
 
-app.post('/items', jsonParser, function(req, res){
-	if(!req.body){
-		return res.sendStatus(400);
-	}
+app.post('/items', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
 
-	var item = storage.add(req.body.name);
-	res.status(201).json(item);
+    var item = storage.add(req.body.name);
+    res.status(201).json(item);
 });
 
-app.delete('/items/:id', jsonParser, function(req,res){
-	storage.delete(req.params.id);
-	console.log("DELETED?!");
-
+app.delete('/items/:id', jsonParser, function(req, res) {
+	console.log(storage.items);
+    if (Number.isInteger(req.params.id)) {
+        return res.sendStatus(400);
+    }
+    for (var i = 0; i < storage.items.length; i++) {
+        if (storage.items[i].id == req.params.id) {
+            res.status(201).json(req.params);
+            return storage.items.splice(i, 1);
+        }
+    }
 });
 
-// app.put('/items/:id', jsonParser, function(req, res){
-// 	if(!req.body){
-// 		return res.sendStatus(400);
-// 	}
-// 	console.log(storage.req.id);
-
-// });
 
 
 
