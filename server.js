02a12101610526b2abfd2701e7b7bfe4110a87e4
Scott Warner <scottbwarner@gmail.com>
2015-10-30 00:43:25 -0400

    begin writing delete req

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var Storage = function() {
     this.items = [];
@@ -12,6 +14,14 @@ Storage.prototype.add = function(name) {
     return item;
 };
 
+Storage.prototype.delete = function(id){
+	
+	console.log(this.items);
+	console.log(this.items[id]);
+	this.items.splice(id,1);
+
+};
+
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
@@ -23,5 +33,30 @@ app.use(express.static('public'));
 app.get('/items', function(req, res) {
     res.json(storage.items);
 });
+
+app.post('/items', jsonParser, function(req, res){
+	if(!req.body){
+		return res.sendStatus(400);
+	}
+
+	var item = storage.add(req.body.name);
+	res.status(201).json(item);
+});
+
+app.delete('/items/:id', jsonParser, function(req,res){
+	storage.delete(req.params.id);
+	console.log("DELETED?!");
+
+});
+
+// app.put('/items/:id', jsonParser, function(req, res){
+// 	if(!req.body){
+// 		return res.sendStatus(400);
+// 	}
+// 	console.log(storage.req.id);
+
+// });
+
+
 
 app.listen(process.env.PORT || 8080);
